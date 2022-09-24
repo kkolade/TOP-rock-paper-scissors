698cff79b92679b0a6ea4466081b2afca3314672
Kola Kolade <kolakolade@gmail.com>
2022-09-24 21:34:02 +0100

    Added the single game round function

--- js/app.js
+++ js/app.js
@@ -6,15 +6,25 @@ const getComputerChoice = () => {
   return choice[rand()];
 };
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+let playerChoice = prompt(
+  "Make a selection: Rock, Paper or Scissors"
+).toLowerCase();
+const singleRound = (playerSelection, computerSelection) => {
+  if (playerSelection === "paper" && computerSelection === "rock") {
+    return `You Win! Paper wraps Rock`;
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return `You Lose! Scissors cuts Paper`;
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return `You Lose! Paper wraps Rock`;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return `You Win! Rock breaks Scissors`;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return `You Win! Paper wraps Rock`;
+  } else if (playerSelection === "scissor" && computerSelection === "rock") {
+    return `You Lose! Rock breaks Scissors`;
+  } else {
+    return `This game is a draw!`;
+  }
+};
+
+console.log(singleRound(playerChoice, getComputerChoice()));
