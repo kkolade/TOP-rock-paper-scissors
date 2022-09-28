64bc56ffa58622891a6bbc4d19893d4ebd5be853
Kola Kolade <kolakolade@gmail.com>
2022-09-28 10:51:56 +0100

    images added as a new folderfor images is created

--- js/app.js
+++ js/app.js
@@ -26,26 +26,26 @@ const playRound = (playerSelection, computerSelection) => {
   }
 };
 
-const game = () => {
-  let score = 0,
-    result;
-  for (let i = 0; i < 5; i++) {
-    let playerChoice = prompt(
-      "Make a selection: Rock, Paper or Scissors"
-    ).toLowerCase();
-    result = playRound(playerChoice, getComputerChoice());
-    if (result.includes("Win")) {
-      score++;
-    }
-    console.log(`${result}`);
-  }
-  if (score >= 3) {
-    console.log(`You Win!, you won ${score} games out of 5`);
-  } else {
-    console.log(
-      `You Lose!, you won ${score} game${score === 1 ? "" : "s"} out of 5`
-    );
-  }
-};
+// const game = () => {
+//   let score = 0,
+//     result;
+//   for (let i = 0; i < 5; i++) {
+//     let playerChoice = prompt(
+//       "Make a selection: Rock, Paper or Scissors"
+//     ).toLowerCase();
+//     result = playRound(playerChoice, getComputerChoice());
+//     if (result.includes("Win")) {
+//       score++;
+//     }
+//     console.log(`${result}`);
+//   }
+//   if (score >= 3) {
+//     console.log(`You Win!, you won ${score} games out of 5`);
+//   } else {
+//     console.log(
+//       `You Lose!, you won ${score} game${score === 1 ? "" : "s"} out of 5`
+//     );
+//   }
+// };
 
-game();
+// game();
